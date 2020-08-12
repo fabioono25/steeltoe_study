e50627ad226d6bdba8f7be6debba080e270166cf
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-08-12 10:48:53 +0100

    add logger actuator

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,6 +27,9 @@ namespace SteeltoePlayground.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+
+            _logger.LogInformation("Hello");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Steeltoe.Management.Tracing;
 
 namespace SteeltoePlayground
 {
@@ -24,6 +19,7 @@ namespace SteeltoePlayground
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedTracing(Configuration);
             services.AddControllers();
         }
 
